b2d4dd413d236ce037e0ee5254a5659f24b70b54
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-08-11 07:06:19 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Web;
+Console.WriteLine("started");
+if (args.Length is not 0 && args[0].StartsWith("cexec:"))
+{
+    Process.Start("explorer.exe", HttpUtility.UrlDecode(args[0][6..]));
+}
+else
+{
+    Registry.SetValue("HKEY_CLASSES_ROOT\\cexec", "", (object)"URL:cexec Protocol");
+    Registry.SetValue("HKEY_CLASSES_ROOT\\cexec", "URL Protocol", (object)"");
+    Registry.SetValue("HKEY_CLASSES_ROOT\\cexec\\shell\\open\\command", "", (object)("\"" + typeof(Program).Assembly.Location + "\" \"%1\""));
+}
