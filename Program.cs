6b00d2ddeab12281cd673a5ed22bb2db136a2d5e
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-08-22 19:28:38 +0300

    Fix CMD

--- Program.cs
+++ Program.cs
@@ -4,11 +4,23 @@ using System.Web;
 Console.WriteLine("started");
 if (args.Length is not 0 && args[0].StartsWith("cexec:"))
 {
-    Process.Start(HttpUtility.UrlDecode(args[0][6..]));
+
+    ProcessStartInfo processStartInfo = new()
+    {
+        FileName = "cmd.exe",
+        Arguments = $"/c {HttpUtility.UrlDecode(args[0][6..])}",
+        UseShellExecute = false,
+        CreateNoWindow = true,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true
+    };
+    Process process = new() { StartInfo = processStartInfo };
+    process.Start();
+    process.WaitForExit();
 }
 else
 {
     Registry.SetValue("HKEY_CLASSES_ROOT\\cexec", "", (object)"URL:cexec Protocol");
     Registry.SetValue("HKEY_CLASSES_ROOT\\cexec", "URL Protocol", (object)"");
-    Registry.SetValue("HKEY_CLASSES_ROOT\\cexec\\shell\\open\\command", "", (object)("\"" + typeof(Program).Assembly.Location + "\" \"%1\""));
+    Registry.SetValue("HKEY_CLASSES_ROOT\\cexec\\shell\\open\\command", "", (object)("\"" + System.AppContext.BaseDirectory + "cexec.exe\"  \"%1\""));
 }
